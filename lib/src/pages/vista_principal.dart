173750eb44e9f831803a47b28fc30484ca579be0
DANIEL SANCHEZ ROBLES <danielsan.robles16@gmail.com>
2022-06-20 19:26:55 -0500

    folder standardization

---
+++ lib/src/pages/vista_principal.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class vista_principal extends StatefulWidget {
+  const vista_principal({Key? key}) : super(key: key);
+
+  @override
+  State<vista_principal> createState() => _vista_principalState();
+}
+
+class _vista_principalState extends State<vista_principal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Vista principal'),
+    );
+  }
+}
