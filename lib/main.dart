82ba5b2df658901768cf57661acddfc74b96dcb4
DANIEL SANCHEZ ROBLES <danielsan.robles16@gmail.com>
2022-07-05 21:59:19 -0500

    vissta 1 registro terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vacoro_proyect/src/pages/registro_user.dart';
 import 'package:vacoro_proyect/src/pages/vista_principal.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       initialRoute: 'vistaPrincipal',
       routes: {
-        'vistaPrincipal': (BuildContext context) => vista_principal(),
+        'vistaPrincipal': (BuildContext context) => registroUser(),
       },
     );
   }
--- lib/src/pages/registro_user.dart
+++ lib/src/pages/registro_user.dart
@@ -56,7 +56,82 @@ class _registroUserState extends State<registroUser> {
           inputs('Correo Electronico', 'Ingrese Correo Electronico'),
           inputs('Contraseña', 'Ingrese contraseña'),
           inputs('Repetir contraseña', 'Repila la contraseña'),
-          drop_button()
+          Row(
+            children: [drop_button(220, "Estado"), drop_button(220, "Ciudad")],
+          ),
+          Container(
+            padding: EdgeInsets.only(left: bordes, right: 250),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 10, bottom: 5),
+                  width: width_media,
+                  child: const Text(
+                    'Edad',
+                    textAlign: TextAlign.left,
+                    // ignore: unnecessary_const
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3E762F),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Color(0xFF68C24E)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xFF3E762F), width: 2.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xFF3E762F), width: 2.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
+                      labelText: 'Ingrese edad',
+                    ),
+                  ),
+                ),
+                const Divider(
+                  height: 5,
+                ),
+              ],
+            ),
+          ),
+          inputs('Ingrese nombre del rancho', 'Nombre del rancho'),
+          Container(
+            height: 70,
+            padding: EdgeInsets.only(
+                left: bordes, right: bordes, bottom: 10, top: 10),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF68C24E)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Siguiente',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
@@ -68,17 +143,18 @@ class _registroUserState extends State<registroUser> {
       child: Column(
         children: [
           Container(
-              padding: const EdgeInsets.only(top: 10, bottom: 5),
-              width: width_media,
-              child: Text(
-                nameTopField,
-                textAlign: TextAlign.left,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF3E762F),
-                ),
-              )),
+            padding: const EdgeInsets.only(top: 10, bottom: 5),
+            width: width_media,
+            child: Text(
+              nameTopField,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF3E762F),
+              ),
+            ),
+          ),
           SizedBox(
             height: 40,
             child: TextField(
@@ -102,39 +178,70 @@ class _registroUserState extends State<registroUser> {
           ),
           const Divider(
             height: 5,
-          )
+          ),
         ],
       ),
     );
   }
 
-  Widget drop_button() {
+  Widget drop_button(
+    int tam_width,
+    String nameTopField,
+  ) {
     String dropdownValue = 'One';
-    return Container(
-      color: Colors.amber,
-      padding: EdgeInsets.only(left: bordes, right: bordes),
-      child: DropdownButton<String>(
-        style: const TextStyle(
-          color: Color(0xFF68C24E),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.only(top: 10, bottom: 5, left: 30),
+          width: width_media - tam_width,
+          child: Text(
+            nameTopField,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF3E762F),
+            ),
+          ),
         ),
-        value: dropdownValue,
-        items: <String>['One', 'Two', 'Free', 'Four']
-            .map<DropdownMenuItem<String>>(
-          (String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          },
-        ).toList(),
-        onChanged: (String? newValue) {
-          setState(
-            () {
-              dropdownValue = newValue!;
-            },
-          );
-        },
-      ),
+        Container(
+            width: width_media - tam_width,
+            padding: EdgeInsets.only(left: bordes),
+            child: SizedBox(
+              height: 40,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  border:
+                      Border.all(color: const Color(0xFF3E762F), width: 2.0),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: DropdownButton<String>(
+                  alignment: AlignmentDirectional.topEnd,
+                  style: const TextStyle(
+                    color: Color(0xFF68C24E),
+                  ),
+                  value: dropdownValue,
+                  items: <String>['One', 'Two', 'Free', 'Four']
+                      .map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (String? newValue) {
+                    setState(
+                      () {
+                        dropdownValue = newValue!;
+                      },
+                    );
+                  },
+                ),
+              ),
+            )),
+      ],
     );
   }
 }
