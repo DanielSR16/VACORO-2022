c1873c71d68a7bb02467842f08772c0888a2d6cc
espdul <193227@ids.upchiapas.edu.mx>
2022-07-09 17:04:04 -0500

    feature/VACORO-59

---
+++ lib/src/pages/anadir_medicamento_animal.dart
@@ -0,0 +1,299 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:vacoro_proyect/src/style/colors/colorview.dart';
+import 'package:intl/intl.dart';
+
+class AnadirMedicamentoAnimal extends StatefulWidget {
+  const AnadirMedicamentoAnimal({Key? key}) : super(key: key);
+
+  @override
+  State<AnadirMedicamentoAnimal> createState() =>
+      _AnadirMedicamentoAnimalState();
+}
+
+class _AnadirMedicamentoAnimalState extends State<AnadirMedicamentoAnimal> {
+  File? image;
+  bool isSwitched = false;
+  TextEditingController descripcion = TextEditingController();
+  TextEditingController dosis = TextEditingController();
+  TextEditingController dateinputFechaAplicacion = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'AGREGAR MEDICAMENTO AL ANIMAL',
+            style: TextStyle(fontSize: 12.0),
+          ),
+        ),
+        leading: SizedBox(
+          child: IconButton(
+            splashRadius: 15,
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+              size: 40,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        actions: [
+          Container(
+            padding: const EdgeInsets.only(left: 30),
+            width: 85,
+            child: Image.asset('assets/images/logo_blanco.png'),
+          )
+        ],
+        backgroundColor: ColorSelect.color5,
+      ),
+      body: SizedBox(
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.only(right: 20, left: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  selectMedicamento("Medicamento", size),
+                  inputs("Descripción", "Ingrese una descripción", size,
+                      descripcion),
+                  inputs("Dosis", "Ingrese la dosis", size, dosis),
+                  date(context, size),
+                  Container(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 280),
+                    child: SizedBox(
+                      width: size.width - 50,
+                      height: 50,
+                      child: ElevatedButton(
+                          child: const Text(
+                            'Agregar',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              primary: ColorSelect.color5,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30)))),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget inputs(String nameTopField, String nameInField, Size size,
+      TextEditingController controllerInput) {
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 10, bottom: 5),
+            width: size.width,
+            child: Text(
+              nameTopField,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF3E762F),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            child: TextField(
+              controller: controllerInput,
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: ColorSelect.color5),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: ColorSelect.color1, width: 2.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: ColorSelect.color5, width: 2.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+                labelText: nameInField,
+              ),
+            ),
+          ),
+          const Divider(
+            height: 5,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget selectMedicamento(String nameTopField, Size size) {
+    var dropdownValue = "Medicamento 1";
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 15),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 10, bottom: 5),
+            width: size.width,
+            child: Text(
+              nameTopField,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: ColorSelect.color1,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 45,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border.all(color: ColorSelect.color1, width: 2),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: DropdownButtonFormField(
+                decoration: const InputDecoration(
+                    enabledBorder: InputBorder.none, fillColor: Colors.white),
+                value: dropdownValue,
+                iconSize: 25,
+                iconEnabledColor: ColorSelect.color1,
+                icon: Container(
+                    margin: const EdgeInsets.only(right: 30),
+                    child: const Icon(Icons.arrow_drop_down)),
+                style: const TextStyle(fontSize: 16, color: Colors.black),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+                items: <String>[
+                  'Medicamento 1',
+                  'Medicamento 2',
+                  'Medicamento 3',
+                  'Medicamento 4'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Container(
+                        margin: const EdgeInsets.only(left: 20),
+                        child: Text(value)),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget date(BuildContext context, Size size) {
+    //Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 10, bottom: 5),
+            width: size.width,
+            child: const Text(
+              "Fecha de aplicación",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF3E762F),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            child: TextField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(
+                  Icons.calendar_today,
+                  color: ColorSelect.color1,
+                ),
+                iconColor: ColorSelect.color1,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: ColorSelect.color1, width: 2.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: ColorSelect.color1, width: 2.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+                labelText: "Seleccionar la fecha",
+                labelStyle: TextStyle(color: ColorSelect.color5),
+              ),
+              controller: dateinputFechaAplicacion,
+              readOnly:
+                  true, //Para que el usuario no pueda editar en el textField
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(
+                      2001, //Fecha limite para seleccionar
+                    ),
+                    lastDate: DateTime(2101),
+                    //Fecha limite para seleccionar
+                    builder: (context, child) {
+                      return Theme(
+                        data: Theme.of(context).copyWith(
+                          colorScheme: const ColorScheme.light(
+                            primary: ColorSelect.color5,
+                            onPrimary: Colors.white,
+                            onSecondary: ColorSelect.color1,
+                          ),
+                          textButtonTheme: TextButtonThemeData(
+                            style: TextButton.styleFrom(
+                              primary: ColorSelect.color1, // button text color
+                            ),
+                          ),
+                        ),
+                        child: child!,
+                      );
+                    });
+                if (pickedDate != null) {
+                  String formattedDate = DateFormat('yyyy-MM-dd')
+                      .format(pickedDate); //La fecha se mostrar en este formato
+                  setState(
+                    () {
+                      dateinputFechaAplicacion.text =
+                          formattedDate; //Fecha de salida en el textField
+                      // print(formattedDate);
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
