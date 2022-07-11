d2404a17cf3da37d4fe1db62975cc4bce6eb156f
Jahr2001 <193213@ids.upchiapas.edu.mx>
2022-07-11 00:00:24 -0500

    feature/VACORO-70

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:vacoro_proyect/src/pages/dashboard_category.dart';
 import 'package:vacoro_proyect/src/pages/registro_user.dart';
 import 'package:vacoro_proyect/src/pages/registro_user2.dart';
 import 'package:vacoro_proyect/src/pages/vista_principal.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'VACORO',
-        initialRoute: 'dash_calf',
+        initialRoute: 'dash_category',
         routes: {
           // 'autenticacion': (BuildContext context) => const autenticacion(),
           // 'homePage': (BuildContext context) => homePage(),
@@ -51,8 +52,9 @@ class MyApp extends StatelessWidget {
           'dash_cow': (BuildContext context) => DashBoardCow(),
           'dash_bull': (BuildContext context) => DashBoardBull(),
           'dash_calf': (BuildContext context) => DashBoardCalf(),
+          'dash_category': (BuildContext context) => DashboardCategory(),
           'registroUser': (BuildContext context) => registroUser(),
-          'registroUser2': (BuildContext context) => registroUser2()
+          'registroUser2': (BuildContext context) => registroUser2(),
         });
   }
 }
