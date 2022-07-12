91590ca968c5b35aa72bf68f810afe34a7aa817c
Jahr2001 <193213@ids.upchiapas.edu.mx>
2022-07-12 04:12:19 -0500

    feature/VACORO-67

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'VACORO',
-        initialRoute: 'splash',
+        initialRoute: 'medication',
         routes: {
           // 'autenticacion': (BuildContext context) => const autenticacion(),
           // 'homePage': (BuildContext context) => homePage(),
