85dcd11fc4f1a7fd5015f8166bf6b2a72f75d6bf
espdul <193227@ids.upchiapas.edu.mx>
2022-07-12 22:50:23 -0500

    feature/VACORO-50

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -37,167 +37,165 @@ class _LoginState extends State<Login> {
         ),
         backgroundColor: ColorSelect.color5,
       ),
-      body: SizedBox(
-        width: size.width,
-        height: size.height,
-        child: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.only(right: 10, left: 10),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: size.height - 500,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/logo.png')),
-                    ),
+      body: SafeArea(
+        child: SizedBox(
+          width: size.width,
+          height: size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 250,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/logo.png')),
                   ),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    margin: const EdgeInsets.only(right: 210),
-                    child: const Text(
-                      'Correo electrónico',
-                      style: TextStyle(
-                          color: ColorSelect.color1,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15),
-                    ),
+                ),
+                Container(
+                  width: size.width * 0.90,
+                  margin: const EdgeInsets.only(top: 20, bottom: 10),
+                  child: const Text(
+                    'Correo electrónico',
+                    style: TextStyle(
+                        color: ColorSelect.color1,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 12, left: 11),
-                    child: TextField(
-                      controller: email,
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(
-                          fontSize: 15, color: ColorSelect.color1),
-                      decoration: const InputDecoration(
-                        fillColor: Colors.amber,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        hintText: 'Ingrese su correo electrónico',
+                ),
+                SizedBox(
+                  width: size.width * 0.90,
+                  child: TextField(
+                    controller: email,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 15, color: ColorSelect.color1),
+                    decoration: const InputDecoration(
+                      fillColor: Colors.amber,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
                       ),
-                      onChanged: (text) {},
+                      hintText: 'Ingrese su correo electrónico',
                     ),
+                    onChanged: (text) {},
                   ),
-                  Container(
-                    margin:
-                        const EdgeInsets.only(right: 260, top: 20, bottom: 15),
-                    child: const Text(
-                      'Contraseña',
-                      style: TextStyle(
-                          color: ColorSelect.color1,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15),
-                    ),
+                ),
+                Container(
+                  width: size.width * 0.90,
+                  margin: const EdgeInsets.only(top: 20, bottom: 10),
+                  child: const Text(
+                    'Contraseña',
+                    style: TextStyle(
+                        color: ColorSelect.color1,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 12, left: 11),
-                    child: TextField(
-                      controller: password,
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(
-                          fontSize: 15, color: ColorSelect.color1),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        hintText: 'Ingrese su contraseña',
+                ),
+                Container(
+                  width: size.width * 0.90,
+                  margin: const EdgeInsets.only(right: 12, left: 11),
+                  child: TextField(
+                    controller: password,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 15, color: ColorSelect.color1),
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
                       ),
-                      onChanged: (text) {},
+                      hintText: 'Ingrese su contraseña',
                     ),
+                    onChanged: (text) {},
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 8),
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      child: const Text(
-                        '¿Haz olvidado tu contraseña?',
-                        style: TextStyle(
+                ),
+                Container(
+                  width: size.width * 0.90,
+                  margin: const EdgeInsets.only(right: 8),
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    child: const Text(
+                      '¿Haz olvidado tu contraseña?',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: ColorSelect.color5,
+                          fontSize: 16),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  width: size.width * 0.90,
+                  padding: const EdgeInsets.only(top: 35),
+                  child: SizedBox(
+                    width: size.width - 50,
+                    height: 50,
+                    child: ElevatedButton(
+                        child: const Text(
+                          'Ingresar',
+                          style: TextStyle(
+                            fontSize: 16,
                             fontWeight: FontWeight.bold,
-                            color: ColorSelect.color5,
-                            fontSize: 16),
-                      ),
-                      onPressed: () {},
-                    ),
+                          ),
+                        ),
+                        onPressed: () {
+                          servicelogin(email, password).then((value) {
+                            if (value['id'] == 'errorEmail') {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  duration: Duration(milliseconds: 1000),
+                                  content: Text('Email incorrecto'),
+                                ),
+                              );
+                            } else if (value['id'] == 'errorPassword') {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  duration: Duration(milliseconds: 1000),
+                                  content: Text('Password incorrecto'),
+                                ),
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  duration: Duration(milliseconds: 1000),
+                                  content: Text('Inicio de sesión correcto'),
+                                ),
+                              );
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        homePage(nombre: value['nombre'])),
+                              );
+                            }
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                            primary: ColorSelect.color5,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)))),
                   ),
-                  Container(
-                    padding: const EdgeInsets.only(top: 35),
-                    child: SizedBox(
-                      width: size.width - 50,
-                      height: 50,
-                      child: ElevatedButton(
-                          child: const Text(
-                            'Ingresar',
-                            style: TextStyle(
-                              fontSize: 16,
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("¿Todavía no tienes una cuenta?",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.left),
+                      TextButton(
+                        child: const Text(
+                          'Regístrate',
+                          style: TextStyle(
                               fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onPressed: () {
-                            servicelogin(email, password).then((value) {
-                              if (value['id'] == 'errorEmail') {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    duration: Duration(milliseconds: 1000),
-                                    content: Text('Email incorrecto'),
-                                  ),
-                                );
-                              } else if (value['id'] == 'errorPassword') {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    duration: Duration(milliseconds: 1000),
-                                    content: Text('Password incorrecto'),
-                                  ),
-                                );
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    duration: Duration(milliseconds: 1000),
-                                    content: Text('Inicio de sesión correcto'),
-                                  ),
-                                );
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          homePage(nombre: value['nombre'])),
-                                );
-                                //Navigator.push(context,homePage(nombre: "f"));
-                              }
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                              primary: ColorSelect.color5,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30)))),
-                    ),
+                              color: ColorSelect.color5,
+                              fontSize: 16),
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
                   ),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text("¿Todavía no tienes una cuenta?",
-                            style: TextStyle(fontSize: 16),
-                            textAlign: TextAlign.left),
-                        TextButton(
-                          child: const Text(
-                            'Regístrate',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: ColorSelect.color5,
-                                fontSize: 16),
-                          ),
-                          onPressed: () {},
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
