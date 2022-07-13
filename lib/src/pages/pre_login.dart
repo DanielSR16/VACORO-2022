bfad2ee7f462fb2ee071a6e5bf777fded754db39
Dulce Maria Fuentes Espinosa <74284122+Espdul@users.noreply.github.com>
2022-07-12 22:48:40 -0500

    feature/VACORO-52_responsive (#19)

--- lib/src/pages/pre_login.dart
+++ lib/src/pages/pre_login.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:vacoro_proyect/src/services/authFacebook.dart';
 import 'package:vacoro_proyect/src/services/authGoogle.dart';
 import '../style/colors/colorview.dart';
@@ -23,199 +24,206 @@ class preLogin extends StatelessWidget {
           ),
           backgroundColor: ColorSelect.color5,
         ),
-        body: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: SafeArea(
+        body: SafeArea(
+          child: SizedBox(
+            width: size.width,
+            height: size.height,
             child: SingleChildScrollView(
-              child: Column(children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                      left: 24, right: 24, top: 20, bottom: 50),
-                  height: size.width - 150,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/logo.png')),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  width: size.width - 75,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            ColorSelect.color3),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                          left: 24, right: 24, top: 20, bottom: 50),
+                      height: 250,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/logo.png')),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      width: size.width * 0.80,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                ColorSelect.color3),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            )),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: const [
+                            Text('Continuar con Google',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: ColorSelect.color6,
+                                    fontSize: 18),
+                                textAlign: TextAlign.center),
+                            Image(
+                                width: 32,
+                                image:
+                                    AssetImage('assets/images/icon_google.png'))
+                          ],
+                        ),
+                        onPressed: () {
+                          try {
+                            signInWithGoogle().then((value) => {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => homePage(
+                                              nombre:
+                                                  value.user!.displayName!)))
+                                });
+                          } catch (e) {
+                            // ignore: avoid_print
+                            print(e);
+                          }
+                        },
+                      ),
+                    ),
+                    Container(
+                        width: size.width * 0.80,
+                        height: 50,
+                        margin: const EdgeInsets.only(top: 20),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  ColorSelect.color5),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                              )),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: const [
+                              Text('Continuar con Facebook',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: ColorSelect.color6),
+                                  textAlign: TextAlign.center),
+                              Image(
+                                  width: 32,
+                                  image: AssetImage(
+                                      'assets/images/icon_facebook.png'))
+                            ],
                           ),
+                          onPressed: () {
+                            try {
+                              signInWithFacebook().then((value) => {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => homePage(
+                                                    nombre: value
+                                                        .user!.displayName!)))
+                                      }
+                                  // ignore: avoid_print
+                                  );
+                            } catch (e) {
+                              // ignore: avoid_print
+                              print(e);
+                            }
+                          },
                         )),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        Text('Continuar con Google',
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: size.width * 0.80,
+                          margin: const EdgeInsets.only(top: 20),
+                          child: const Text(
+                            '¿Ya tienes una cuenta?',
                             style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: ColorSelect.color6,
-                                fontSize: 18),
-                            textAlign: TextAlign.center),
-                        Image(
-                            width: 32,
-                            image: AssetImage('assets/images/icon_google.png'))
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
-                    onPressed: () {
-                      try {
-                        signInWithGoogle().then((value) => {
-                              Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => homePage(
-                                          nombre: value.user!.displayName!)))
-                            });
-                      } catch (e) {
-                        // ignore: avoid_print
-                        print(e);
-                      }
-                    },
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 150, top: 20),
-                  child: const Text(
-                    '¿Ya tienes una cuenta?',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                Container(
-                    width: size.width - 75,
-                    height: 50,
-                    margin: const EdgeInsets.only(top: 20),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              ColorSelect.color5),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          )),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: const [
-                          Text('Continuar con Facebook',
-                              style: TextStyle(
+                    Container(
+                      width: size.width * 0.80,
+                      height: 50,
+                      margin: const EdgeInsets.only(top: 5),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                ColorSelect.color1),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            )),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            Text('Iniciar sesión',
+                                style: TextStyle(
                                   fontSize: 18,
                                   fontWeight: FontWeight.bold,
-                                  color: ColorSelect.color6),
-                              textAlign: TextAlign.center),
-                          Image(
-                              width: 32,
-                              image:
-                                  AssetImage('assets/images/icon_facebook.png'))
-                        ],
+                                  color: ColorSelect.color6, //
+                                ),
+                                textAlign: TextAlign.center),
+                          ],
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'login');
+                        },
                       ),
-                      onPressed: () {
-                        try {
-                          signInWithFacebook().then((value) => {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => homePage(
-                                                nombre:
-                                                    value.user!.displayName!)))
-                                  }
-                              // ignore: avoid_print
-                              );
-                        } catch (e) {
-                          // ignore: avoid_print
-                          print(e);
-                        }
-                      },
-                    )),
-                Container(
-                  margin: const EdgeInsets.only(right: 150, top: 20),
-                  child: const Text(
-                    '¿Ya tienes una cuenta?',
-                    style: TextStyle(
-                      fontSize: 16,
                     ),
-                  ),
-                ),
-                Container(
-                  width: size.width - 75,
-                  height: 50,
-                  margin: const EdgeInsets.only(top: 5),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            ColorSelect.color1),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                          ),
-                        )),
-                    child: Row(
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        Text('Iniciar sesión',
+                      children: [
+                        Container(
+                          width: size.width * 0.80,
+                          margin: const EdgeInsets.only(top: 20),
+                          child: const Text(
+                            '¿Aún no tienes una cuenta?',
                             style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: ColorSelect.color6, //
+                              fontSize: 16,
                             ),
-                            textAlign: TextAlign.center),
+                          ),
+                        ),
                       ],
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'login');
-                    },
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 115, top: 20),
-                  child: const Text(
-                    '¿Aún no tienes una cuenta?',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                Container(
-                    width: size.width - 75,
-                    height: 50,
-                    margin: const EdgeInsets.only(top: 5),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              ColorSelect.color4),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          )),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: const [
-                          Text('Registrate',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: ColorSelect.color6, //
-                              ),
-                              textAlign: TextAlign.center),
-                        ],
-                      ),
-                      onPressed: () {},
-                    ))
-              ]),
+                    Container(
+                        width: size.width * 0.80,
+                        height: 50,
+                        margin: const EdgeInsets.only(top: 5, bottom: 20),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  ColorSelect.color4),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                              )),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: const [
+                              Text('Registrate',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: ColorSelect.color6, //
+                                  ),
+                                  textAlign: TextAlign.center),
+                            ],
+                          ),
+                          onPressed: () {},
+                        ))
+                  ]),
             ),
           ),
         ),
