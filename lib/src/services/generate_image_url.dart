cb33a9c2f201e5b6c0cf2c60f728cea4295ced82
Dulce Maria Fuentes Espinosa <74284122+Espdul@users.noreply.github.com>
2022-07-15 16:43:27 -0500

    Feature/vacoro 55 (#36)
    * feature/VACORO-55_se agrego controller
    
    * feature/VACORO-55_se agrego controller
    
    * feature/VACORO-55_correccion
    
    * feature/VACORO-55_agregaranimal
    
    * feature/VACORO-55_completo
    
    * feature/VACORO-55_completo
    
    * feature/VACORO-55_completo
    
    Co-authored-by: DanielSR16 <danielsan.robles16@gmail.com>

--- lib/src/services/generate_image_url.dart
+++ lib/src/services/generate_image_url.dart
@@ -8,30 +8,30 @@ class GenerateImageUrl {
   late bool isGenerated;
   late String uploadUrl;
   late String downloadUrl;
-  String ip = '192.168.56.1';
+  String ip = '192.168.0.3';
   Future<void> call(String fileType) async {
-    try {
-      Map body = {"fileType": fileType};
+    // try {
+    Map body = {"fileType": fileType};
 
-      var response = await http.post(
-        Uri.http(ip + ':5000', '/generatePresignedUrl'),
-        body: body,
-      );
+    var response = await http.post(
+      Uri.http(ip + ':5000', '/generatePresignedUrl'),
+      body: body,
+    );
 
-      var result = jsonDecode(response.body);
+    var result = jsonDecode(response.body);
 
-      if (result['success'] != null) {
-        success = result['success'];
-        message = result['message'];
+    if (result['success'] != null) {
+      success = result['success'];
+      message = result['message'];
 
-        if (response.statusCode == 201) {
-          isGenerated = true;
-          uploadUrl = result["uploadUrl"];
-          downloadUrl = result["downloadUrl"];
-        }
+      if (response.statusCode == 201) {
+        isGenerated = true;
+        uploadUrl = result["uploadUrl"];
+        downloadUrl = result["downloadUrl"];
       }
-    } catch (e) {
-      throw ('Error getting url');
     }
+    // } catch (e) {
+    //   throw ('Error getting url');
+    // }
   }
 }
